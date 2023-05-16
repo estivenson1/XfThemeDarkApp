ca9a9ed0ab88aa2b63f450b99b11cdd047f37f9e
Estivenson1 <Estivenson1@hotmail.com>
2023-05-16 16:42:01 -0500

    Creación del Proyecto

---
+++ XfThemeDarkApp/XfThemeDarkApp/Views/NewItemPage.xaml.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using Xamarin.Forms;
+using Xamarin.Forms.Xaml;
+
+using XfThemeDarkApp.Models;
+using XfThemeDarkApp.ViewModels;
+
+namespace XfThemeDarkApp.Views
+{
+    public partial class NewItemPage : ContentPage
+    {
+        public Item Item { get; set; }
+
+        public NewItemPage()
+        {
+            InitializeComponent();
+            BindingContext = new NewItemViewModel();
+        }
+    }
+}
